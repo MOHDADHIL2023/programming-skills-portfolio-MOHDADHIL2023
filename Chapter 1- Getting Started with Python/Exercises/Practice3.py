26961f80739e904ada928d7510870ab9aeabecf2
MOHDADHIL2023 <144331717+MOHDADHIL2023@users.noreply.github.com>
2023-10-11 23:31:11 +0400

    Practice3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice3.py
@@ -0,0 +1,10 @@
+#3
+'''age = input ("Enter your age : ")
+print (age)'''
+age = int (input ("Enter Your Age : "))
+print (age)
+print ("my name is Adil")
+print ("my age is 18")
+ans = ((2*3/8+\
+         (23+5)))
+print(int (ans))
