232f5edd8201a0f981b97b5ff00e1dad2c2e17a9
MOHDADHIL2023 <144331717+MOHDADHIL2023@users.noreply.github.com>
2023-10-11 23:36:55 +0400

    Practice4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice4.py
@@ -0,0 +1,9 @@
+#4
+print("Name" , ": Adil " ,12345)
+print("Father's Name" , ": Abul" , 6789)
+Name = "Adil"
+Name = "Bathspa"
+print(Name)
+print(15/2)
+Name = input ("Enter Name: ")
+print (Name)
