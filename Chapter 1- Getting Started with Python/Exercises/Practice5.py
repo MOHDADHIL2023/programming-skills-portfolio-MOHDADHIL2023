dd225e43960e9ee98d3b65112311e13c167b1525
MOHDADHIL2023 <144331717+MOHDADHIL2023@users.noreply.github.com>
2023-10-11 23:54:26 +0400

    Practice5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Practice5.py
@@ -0,0 +1,7 @@
+#5:Write four string in different variables and print the output as one string.
+string1="Hi"
+string2=" Abdul"
+string3=" What's up"
+string4=" How Are You!"
+output_string= string1 + string2 + string3 + string4
+print(output_string)
