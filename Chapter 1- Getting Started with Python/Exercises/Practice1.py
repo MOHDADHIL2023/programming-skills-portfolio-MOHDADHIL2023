d8d0dc57f76c4a99eecac3c49f8cb34ae35d605f
MOHDADHIL2023 <144331717+MOHDADHIL2023@users.noreply.github.com>
2023-10-11 22:02:09 +0400

    Practice1.py

--- Chapter 1- Getting Started with Python/Exercises/Practice1.py
+++ Chapter 1- Getting Started with Python/Exercises/Practice1.py
@@ -1,4 +1,4 @@
-#1:write a program to output the letters W X Y Z , each on a separate line
+#1:Write a program to output the letters W X Y Z , each on a separate line
 print(""" W
           X
           Y
