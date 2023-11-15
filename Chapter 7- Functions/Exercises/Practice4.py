9ef97f1d26cb73a3ff4e41305c06a6485a9e8e7b
MOHDADHIL2023 <144331717+MOHDADHIL2023@users.noreply.github.com>
2023-11-16 00:35:32 +0400

    Practice4.py

---
+++ Chapter 7- Functions/Exercises/Practice4.py
@@ -0,0 +1,12 @@
+#Write a Python program that defines a function to calculate the area of a circle, and then
+#calls that function with a given radius
+
+
+
+import math
+def calculate_area(radius):
+    area = math.pi * radius**2
+    return area
+radius = float(input("Please enter the Radius of the Circle: "))
+area = calculate_area(radius)
+print(f"The area of a circle with radius {radius} is {area}")
