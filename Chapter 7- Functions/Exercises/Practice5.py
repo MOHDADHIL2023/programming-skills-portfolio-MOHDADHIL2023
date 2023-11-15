e469ea233d049bd4a8999f047c08835cef1b7957
MOHDADHIL2023 <144331717+MOHDADHIL2023@users.noreply.github.com>
2023-11-16 00:35:45 +0400

    Practice5.py

---
+++ Chapter 7- Functions/Exercises/Practice5.py
@@ -0,0 +1,9 @@
+#Write a Python program that defines a function to check whether a given number is prime.
+
+def is_prime(num): 
+    if num <= 1: 
+        return False 
+    for i in range(2, int(num**0.5) + 1): 
+        if num % i == 0: 
+            return False 
+        return True
